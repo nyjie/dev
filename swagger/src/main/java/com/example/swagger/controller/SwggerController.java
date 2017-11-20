1f8b287cc58065652d8564be2a50e3ee094b9462
nyjie <2272978668@qq.com>
2017-11-20 14:54:22 +0800

    添加 swagger-ui

---
+++ swagger/src/main/java/com/example/swagger/controller/SwggerController.java
@@ -0,0 +1,11 @@
+package com.example.swagger.controller;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Api(description="这个controller是干嘛的")
+//@ApiOperation(value="这个方法是干嘛的", notes="详细注释")
+public class SwggerController {
+}
